d5058b69b81e9d972d6b55314f01179b59413faf
squinfester <squinfester@gmail.com>
2023-06-05 09:50:26 +0200

    change facicon and initially make the contact section

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -2,22 +2,45 @@ import Heading from "./Heading";
 import HomeInfoSections from "./HomeInfoSections";
 
 import { BiPhoneCall } from "react-icons/bi";
-import { BsFillSignpostFill } from "react-icons/bs";
+import { AiFillHome } from "react-icons/ai";
+
+import { FaFacebook, FaInstagramSquare, FaTiktok } from "react-icons/fa";
 
 const ContactSection = () => {
   return (
     <HomeInfoSections>
       <Heading title="Kontakt" />
-      <section className="flex flex-col gap-6  text-lg">
-        <div className="mx-auto flex flex-col gap-2 text-left">
-          <h2 className="text-center text-2xl">Jeremi BORACKI</h2>
-          <a href="tel:505-795-006" className="flex items-center gap-2">
-            <BiPhoneCall />
+      <h2 className="mb-4 text-center text-2xl">Jeremi BORACKI</h2>
+      <section className="mx-auto flex max-w-sm flex-col items-center justify-center gap-4 text-lg md:flex-row md:gap-2">
+        <div className="flex w-1/2 flex-col gap-4 text-left md:gap-2">
+          <a
+            href="https://www.facebook.com/"
+            className="flex items-center gap-2"
+          >
+            <FaFacebook />
+            facebook.com
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            className="flex items-center gap-2"
+          >
+            <FaInstagramSquare />
+            instagram.com
+          </a>
+          <a href="https://www.tiktok.com/" className="flex items-center gap-2">
+            <FaTiktok />
+            tiktok.com
+          </a>
+        </div>
+        <div className="flex w-1/2 flex-col gap-4 text-left md:gap-2">
+          <a href="tel:505-795-006" className="flex items-center gap-2 ">
+            <BiPhoneCall className="text-xl" />
             <span className="underline">505-795-006</span>
           </a>
           <h2 className="flex items-center gap-2">
-            <BsFillSignpostFill />
-            Nowe tłoki; 64-200 Wolsztyn
+            <AiFillHome className="text-xl" />
+            Nowe tłoki; <br />
+            64-200 Wolsztyn
           </h2>
         </div>
       </section>
