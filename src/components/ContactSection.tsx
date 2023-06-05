ff5f85159d78a68314a1dc03698d0d27c0c0cdea
squinfester <squinfester@gmail.com>
2023-06-05 10:49:34 +0200

    the contact section is done

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -11,37 +11,42 @@ const ContactSection = () => {
     <HomeInfoSections>
       <Heading title="Kontakt" />
       <h2 className="mb-4 text-center text-2xl">Jeremi BORACKI</h2>
-      <section className="mx-auto flex max-w-sm flex-col items-center justify-center gap-4 text-lg md:flex-row md:gap-2">
-        <div className="flex w-1/2 flex-col gap-4 text-left md:gap-2">
-          <a
-            href="https://www.facebook.com/"
-            className="flex items-center gap-2"
-          >
-            <FaFacebook />
-            facebook.com
-          </a>
-          <a
-            href="https://www.instagram.com/"
-            className="flex items-center gap-2"
-          >
-            <FaInstagramSquare />
-            instagram.com
-          </a>
-          <a href="https://www.tiktok.com/" className="flex items-center gap-2">
-            <FaTiktok />
-            tiktok.com
-          </a>
+      <section className="flex items-start justify-center gap-4 text-lg">
+        <div className="flex w-1/2  justify-end text-left md:gap-2">
+          <div className="flex flex-col gap-4 md:gap-2">
+            <a
+              href="https://www.facebook.com/"
+              className="flex items-center gap-2"
+            >
+              <FaFacebook />
+              facebook.com
+            </a>
+            <a
+              href="https://www.instagram.com/"
+              className="flex items-center gap-2"
+            >
+              <FaInstagramSquare />
+              instagram.com
+            </a>
+            <a
+              href="https://www.tiktok.com/"
+              className="flex items-center gap-2"
+            >
+              <FaTiktok />
+              tiktok.com
+            </a>
+          </div>
         </div>
         <div className="flex w-1/2 flex-col gap-4 text-left md:gap-2">
           <a href="tel:505-795-006" className="flex items-center gap-2 ">
             <BiPhoneCall className="text-xl" />
             <span className="underline">505-795-006</span>
           </a>
-          <h2 className="flex items-center gap-2">
+          <h1 className="flex items-center gap-2">
             <AiFillHome className="text-xl" />
-            Nowe tłoki; <br />
+            Nowe tłoki; <br className="md:hidden" />
             64-200 Wolsztyn
-          </h2>
+          </h1>
         </div>
       </section>
     </HomeInfoSections>
