14ce55fb8c20a5cdcca4ca2fda0404499faebdcb
SquinFester <bartoszprzymencki@gmail.com>
2023-07-03 07:13:12 +0200

    as

---
+++ next-try/src/lib/emailjsConfig.ts
@@ -0,0 +1,7 @@
+const emailjsConfig = {
+  service: "service_stqbg3s",
+  temple: "template_289gshc",
+  api: "-Bz90bhoChewjmhy0",
+};
+
+export default emailjsConfig;
---
+++ next-try/src/lib/firebase.ts
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCrlTd69mX2tf5XyOyucb1BptyBjvQN3ug",
+  authDomain: "jeremiboracki.firebaseapp.com",
+  projectId: "jeremiboracki",
+  storageBucket: "jeremiboracki.appspot.com",
+  messagingSenderId: "526442525300",
+  appId: "1:526442525300:web:ef4c4962c6fcf39f521721",
+};
+
+export default FIREBASE_CONFIG;
