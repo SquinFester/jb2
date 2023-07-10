1db05865e63d5412ce3103cbac7eca9989ed0480
bprzymenckideveloart <137422584+bprzymenckideveloart@users.noreply.github.com>
2023-07-10 15:01:30 +0200

    add logo nav in better quality

--- next-try/src/components/Navbar.tsx
+++ next-try/src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     <header className="sticky top-0 z-50 w-full bg-primary shadow-md lg:flex lg:items-center lg:justify-around">
       <div className=" relative z-50 flex w-full justify-between bg-primary px-20 py-6 lg:w-fit lg:px-0">
         <Link href="/">
-          <Image src="/images/logoNav.svg" alt="log" width={180} height={180} />
+          <Image src="/images/logoNav.png" alt="log" width={140} height={140} />
         </Link>
         <HamburgerMenu
           visable={isActive}
