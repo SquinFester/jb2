03dd25e1b7a6e4c3a4b8e13e9e5d25c9daaab77e
bprzymenckideveloart <137422584+bprzymenckideveloart@users.noreply.github.com>
2023-07-11 19:50:12 +0200

    fk this env

--- next-try/src/lib/getImages.ts
+++ next-try/src/lib/getImages.ts
@@ -4,16 +4,7 @@ import FIREBASE_CONFIG from "./firebase";
 
 // Initialize Firebase
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.NFIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_SORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSSING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FIREBASE_CONFIG);
 
 export const storage = getStorage(app);
 
